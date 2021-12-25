c3e7b01ef7c139d8754dfb549b2021cb233ca02d
sivakumar121 <89797369+sivakumar121@users.noreply.github.com>
2021-12-25 22:33:30 +0530

    Add files via upload
    saketha is my girlfriend try to understand

---
+++ sak19.py
@@ -0,0 +1,81 @@
+#pip install speechrecognition
+
+#pip install pyttsx3
+
+#pip install pywhatkit
+
+#pip install 
+
+#pip install wikipedia
+
+#pip install pyjokes
+
+
+#run this commands in our ide's or code Editors
+
+
+
+
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import wikipedia
+import datetime
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+engine.say("hello dear")
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'saketha' in command:
+                command = command.replace('siri', '')
+                print(command)
+
+    except:
+        pass
+    return command
+
+
+def run_saketha():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'are you single' in command:
+        talk("NO , iam... already....in.... love..... with ....siva")
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_saketha()
